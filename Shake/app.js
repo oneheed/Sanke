c58fd386aaf6ac93146072fb4ee0d1ed40d57fdf
grape <oneheed@gmail.com>
2016-11-15 02:06:46 +0800

    update

--- Shake/app.js
+++ Shake/app.js
@@ -1,34 +1,70 @@
-var Greeter = (function () {
-    function Greeter(element) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
-    }
-    Greeter.prototype.start = function () {
-        var _this = this;
-        this.timerToken = setInterval(function () { return _this.span.innerHTML = new Date().toUTCString(); }, 500);
-    };
-    Greeter.prototype.stop = function () {
-        clearTimeout(this.timerToken);
-    };
-    return Greeter;
-}());
+var snakeSectinNum = 5;
+var snakeSpacer = 6;
+var snakeHead;
+var snakeSection = [];
+var snakePath = [];
 var SimpleGame = (function () {
     function SimpleGame() {
         this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {
-            preload: this.preload, create: this.create, update: this.update
+            preload: this.preload,
+            create: this.create,
+            update: this.update,
+            move: this.move
         });
     }
     SimpleGame.prototype.preload = function () {
-        this.game.load.image('logo', 'phaser2.png');
+        this.game.load.crossOrigin = "anonymous";
+        this.game.load.spritesheet('food', 'http://s.ntustcoding.club/snake/foods.png', 36, 36);
+        this.game.load.image('ball', 'http://s.ntustcoding.club/snake/body.png');
+        this.game.load.image('ground', 'http://s.ntustcoding.club/snake/ground.png');
     };
     SimpleGame.prototype.create = function () {
-        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
+        //背景(tileSprite)
+        this.game.add.tileSprite(0, 0, 4000, 4000, 'ground');
+        //邊界
+        this.game.world.setBounds(0, 0, 4000, 4000);
+        //畫蛇頭(sprite 位置 x y 圖片來源)
+        snakeHead = this.game.add.sprite(400, 300, 'ball');
+        snakeHead.anchor.setTo(0.5, 0.5);
+        for (var i = 0; i < snakeSectinNum; i++) {
+            snakeSection[i] = this.game.add.sprite(400 - 10 * i, 300, 'ball');
+            snakeSection[i].anchor.setTo(0.5, 0.5);
+        }
+        //物理引擎(啟用物理引擎)
+        this.game.physics.enable(snakeHead, Phaser.Physics.ARCADE);
+        //是否蛇頭穿越邊界
+        snakeHead.body.collideWorldBounds = true;
+        //照相機
+        this.game.camera.follow(snakeHead);
+    };
+    SimpleGame.prototype.move = function () {
+        // 改變蛇頭方向,自轉角度(自己角度)
+        //snakeHead.rotation += 0.1;
+        //console.log(this.snakeSpacer);
+        //取得面相滑鼠的方位
+        snakeHead.rotation = this.game.physics.arcade.angleToPointer(snakeHead);
+        //蛇頭前進(速度)
+        if (this.game.input.activePointer.leftButton.isDown) {
+            snakeHead.body.velocity = this.game.physics.arcade.velocityFromRotation(snakeHead.rotation, 500);
+        }
+        else {
+            snakeHead.body.velocity = this.game.physics.arcade.velocityFromRotation(snakeHead.rotation, 200);
+        }
+        var point = new Phaser.Point(snakeHead.x, snakeHead.y);
+        snakePath.unshift(point);
+        if (snakeSectinNum * snakeSpacer < snakePath.length) {
+            snakePath.pop();
+        }
+        var tempSectinNum = Math.floor(snakePath.length / snakeSpacer);
+        for (var i = 0; i < snakeSectinNum; i++) {
+            var snakeIndex = tempSectinNum == 0 ? 0 : (i + 1) * tempSectinNum - 1;
+            snakeSection[i].rotation = snakeHead.rotation;
+            snakeSection[i].x = snakePath[snakeIndex].x;
+            snakeSection[i].y = snakePath[snakeIndex].y;
+        }
     };
     SimpleGame.prototype.update = function () {
+        this.move();
     };
     return SimpleGame;
 }());
--- Shake/app.ts
+++ Shake/app.ts
@@ -1,12 +1,20 @@
-﻿class SimpleGame {  
+﻿let snakeSectinNum: number = 5;
+let snakeSpacer: number = 6;
+let snakeHead: Phaser.Sprite;
+let snakeSection: Array<Phaser.Sprite> = [];
+let snakePath: Array<Phaser.Point> = [];
+
+class SimpleGame {  
+    game: Phaser.Game;
+   
     constructor() {
         this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {
-            preload: this.preload, create: this.create, update: this.update
+            preload: this.preload,
+            create: this.create,
+            update: this.update,
+            move: this.move
         });
     }
-
-    game: Phaser.Game;
-    snakeHead: Phaser.Sprite;
 
     preload() {
         this.game.load.crossOrigin = "anonymous";
@@ -16,29 +24,63 @@
     }
 
     create() {
-        var game = this.game;
         //背景(tileSprite)
-        game.add.tileSprite(0, 0, 4000, 4000, 'ground');
+        this.game.add.tileSprite(0, 0, 4000, 4000, 'ground');
         //邊界
-        game.world.setBounds(0, 0, 4000, 4000);
+        this.game.world.setBounds(0, 0, 4000, 4000);
 
         //畫蛇頭(sprite 位置 x y 圖片來源)
-        this.snakeHead = game.add.sprite(400, 300, 'ball');
-        this.snakeHead.anchor.setTo(0.5, 0.5);
+        snakeHead = this.game.add.sprite(400, 300, 'ball'); 
+        snakeHead.anchor.setTo(0.5, 0.5);
 
         for (var i = 0; i < snakeSectinNum; i++) {
-            snakeSection[i] = game.add.sprite(400, 300, 'ball');
+            snakeSection[i] = this.game.add.sprite(400-10*i, 300, 'ball');
             snakeSection[i].anchor.setTo(0.5, 0.5);
+        }
 
-      //var point = new Phaser.Point(snakeSection[i].x, snakeSection[i].y);
-      //snakePath[i] = point;
-      //snakePath[i] = snakeSection[i];
-      //console.log(snakePath[i].x, snakePath[i].y);
-      //game.physics.enable(snakeSection[i], Phaser.Physics.ARCADE);
+        //物理引擎(啟用物理引擎)
+        this.game.physics.enable(snakeHead, Phaser.Physics.ARCADE);
+
+        //是否蛇頭穿越邊界
+        snakeHead.body.collideWorldBounds = true;
+
+        //照相機
+        this.game.camera.follow(snakeHead);
+    }
+
+    move() {
+        // 改變蛇頭方向,自轉角度(自己角度)
+        //snakeHead.rotation += 0.1;
+        //console.log(this.snakeSpacer);
+        //取得面相滑鼠的方位
+        snakeHead.rotation = this.game.physics.arcade.angleToPointer(snakeHead);
+
+        //蛇頭前進(速度)
+        if (this.game.input.activePointer.leftButton.isDown) {
+            snakeHead.body.velocity = this.game.physics.arcade.velocityFromRotation(snakeHead.rotation, 500);
+        } else {
+            snakeHead.body.velocity = this.game.physics.arcade.velocityFromRotation(snakeHead.rotation, 200);
+        }
+
+        var point = new Phaser.Point(snakeHead.x, snakeHead.y);
+        snakePath.unshift(point);
+
+        if (snakeSectinNum * snakeSpacer < snakePath.length) {
+            snakePath.pop();
+        }
+
+        var tempSectinNum = Math.floor(snakePath.length / snakeSpacer);
+
+        for (var i = 0; i < snakeSectinNum; i++) {
+            var snakeIndex = tempSectinNum == 0 ? 0 : (i + 1) * tempSectinNum - 1;
+            snakeSection[i].rotation = snakeHead.rotation;
+            snakeSection[i].x = snakePath[snakeIndex].x;
+            snakeSection[i].y = snakePath[snakeIndex].y;
+        }
     }
 
     update() {
-
+        this.move();
     }
 }
 
